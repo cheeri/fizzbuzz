162072ab7c846bdda8fc8552770cab3970241087
murali.ambekar@gmail.com <murali.ambekar@gmail.com>
2015-10-11 16:09:02 -0500

    refactored fizzbuzz

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,38 +1,55 @@
 $( document ).ready(function() {
     
+    $('#fizzbizzcalculate').click(function(){
 
-        var divideby3 ;
-        var divideby5 ;
-        var display;
+        var fizzbizzvalue;
 
-    	
+        //$('#itemlist').remove();
 
-        for(counter = 1; counter<=100; counter++){
-
-            divideby3 = counter % 3;
-            divideby5 = counter % 5;
-            
-            if (divideby5 === 0 && divideby3 === 0)
+        fizzbizzvalue = $('#fizzbuzzcounter').val();
+        fizzbizzvalue = parseInt(fizzbizzvalue,10); 
+        if (!isNaN(fizzbizzvalue))
             {
-                display = "fizzbuzz";
+                fizzbizzgenerator(fizzbizzvalue );
             }
-            else if (divideby3 === 0) {
-                display = "fizz";
-            }
-            else if (divideby5 === 0) {
-                display = "buzz";
-            }
-            else display = counter;
-
-            display = display.toString();
             
-            $('.content ul').append("<li><label>"+display+"</label></li>");   
-           
 
-        };
-    	
+    });
     		
-   	});   	   
+});  
+
+function fizzbizzgenerator(uppercountervalue ) {
+            
+            var divideby3 ;
+            var divideby5 ;
+            var display;
+            var counter;
+            
+
+            for(counter = 1; counter<=uppercountervalue; counter++){
+
+                divideby3 = counter % 3;
+                divideby5 = counter % 5;
+                
+                if (divideby5 === 0 && divideby3 === 0)
+                {
+                    display = "fizzbuzz";
+                }
+                else if (divideby3 === 0) {
+                    display = "fizz";
+                }
+                else if (divideby5 === 0) {
+                    display = "buzz";
+                }
+                else display = counter;
+
+                display = display.toString();
+                
+                $('ul').append('<li><label>'+display+'</label></li>');  
+         
+            };
+
+   }; 	   
 
     
 
